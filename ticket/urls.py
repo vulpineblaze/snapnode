b41ad5845ab4af656f70fe6d752f795ddaf7b753
Ubuntu <ubuntu@ip-172-31-42-203.us-west-2.compute.internal>
2014-11-15 23:49:48 +0000

    scaffold ticket

--- ticket/urls.py
+++ ticket/urls.py
@@ -3,8 +3,29 @@ from django.conf.urls import patterns, url
 from ticket import views ###
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    # # ex: /polls/5/
+    url(r'^$', views.home, name='home'),    
+
+    # url(r'^home/$', views.home, name='home'),
+    
+    url(r'^index/$', views.index, name='index'),
+    
+    url(r'^new_ticket/$', views.new_ticket, name='new_ticket'),
+    
+    url(r'^detail/$', views.detail, name='detail'),
+    
+    url(r'^edit/$', views.edit, name='edit'),
+    
+    url(r'^new_event/$', views.new_event, name='new_event'),
+    
+    # url(r'^index/$', views.index, name='index'),
+    
+    # url(r'^index/$', views.index, name='index'),
+    
+    # url(r'^index/$', views.index, name='index'),
+    
+    # url(r'^index/$', views.index, name='index'),
+
+    # # # ex: /polls/5/
     # url(r'^(?P<node_id>\d+)/$', views.detail, name='detail'),
     # # ex: /polls/5/results/
     # url(r'^(?P<node_id>\d+)/results/$', views.results, name='results'),
@@ -13,6 +34,7 @@ urlpatterns = patterns('',
 
     # url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
     # url(r'^login/$', views.user_login, name='login'),
+    # url(r'^logout/$', views.user_logout, name='logout'),
     # url(r'^logout/$', views.user_logout, name='logout'),
 
 )
--- ticket/views.py
+++ ticket/views.py
@@ -8,6 +8,7 @@ from core.models import Node, UserProfile
 
 
 def index(request):
+    """  Front page for hot/new tickets. """
     node_list = []
     latest_node_list = Node.objects.order_by('-date_updated')
     # template = loader.get_template('core/index.html')
@@ -23,3 +24,83 @@ def index(request):
 
     context = {'latest_node_list': latest_node_list}
     return render(request, 'ticket/index.html', context)
+
+
+
+def home(request):
+    """  Starting page where User chooses what to do. """
+    
+    generic_html_dump = ""
+
+    generic_html_dump += "<P> In home.html </P>"
+    generic_html_dump += "<a href=\"index\" >INDEX</a><BR>"
+    generic_html_dump += "<a href=\"new_ticket\" >NEW TICKET</a><BR>"
+    generic_html_dump += "<a href=\"index\" >UNDECIDED FEATURE</a><BR>"
+    generic_html_dump += "<a href=\"index\" >UNDECIDED FEATURE</a><BR>"
+
+    context = {'generic_html_dump': generic_html_dump}
+
+    return render(request, 'core/generic.html', context)
+
+
+def new_ticket(request):
+    """  Page for making new tickets. """
+
+    
+    generic_html_dump = ""
+
+    generic_html_dump += "<P> In new_ticket </P>"
+    generic_html_dump += "fake <a href=\"../detail\" >SUBMIT</a>"
+    generic_html_dump += " to pretend we just made a ticket<BR>"
+
+    context = {'generic_html_dump': generic_html_dump}
+
+    return render(request, 'core/generic.html', context)
+
+
+
+
+def detail(request):
+    """  Page for viewing all aspects of a ticket. """
+
+    
+    generic_html_dump = ""
+
+    generic_html_dump += "<P> In detail </P>"
+    generic_html_dump += "<a href=\"../edit\" >EDIT</a><BR>"
+    generic_html_dump += "<a href=\"../new_event\" >NEW_EVENT</a><BR>"
+
+    context = {'generic_html_dump': generic_html_dump}
+
+    return render(request, 'core/generic.html', context)
+
+
+def edit(request):
+    """  Page for editing all aspects of a ticket. """
+
+    
+    generic_html_dump = ""
+
+    generic_html_dump += "<P> In edit </P>"
+    generic_html_dump += "fake <a href=\"../detail\" >SUBMIT</a>"
+    generic_html_dump += " to pretend we just updated a ticket<BR>"
+
+    context = {'generic_html_dump': generic_html_dump}
+
+    return render(request, 'core/generic.html', context)
+
+
+
+def new_event(request):                         ###
+    """  Log a new event under a ticket. """
+
+    
+    generic_html_dump = ""
+
+    generic_html_dump += "<P> In new_event </P>"
+    generic_html_dump += "fake <a href=\"../detail\" >SUBMIT</a>"
+    generic_html_dump += " to pretend we just added an event to a ticket<BR>"
+
+    context = {'generic_html_dump': generic_html_dump}
+
+    return render(request, 'core/generic.html', context)
